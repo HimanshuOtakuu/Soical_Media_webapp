6572b675c200a444eaf8ac5cd258d5f81d770276
HimanshuOtakuu <hs1925846@gmail.com>
2021-06-22 09:55:23 +0530

    set up the view engine using ejs

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8000;
 //using express.router
 app.use('/', require('./routes'));
 
+//set up the view engin
+app.set('view engine', 'ejs');
+app.set('views', './views'); 
 
 
 app.listen(port, function(err){
